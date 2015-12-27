8856fc2f92e182b1c715b9cfd67ae75f5a98fe28
xpeper <xpepermint@gmail.com>
2015-12-27 23:53:33 +0100

    enabled only looping bug fix

--- example/index.js
+++ example/index.js
@@ -37,7 +37,7 @@ let cron = Note.createCron().start();
 Checklist.findOneAndUpdate({_id: '565781bba17d0e685f8e2086'}, {
   name: 'Job 1',
   description: 'ignored by the cron heartbit'
-}, {upsert: true, setDefaultsOnInsert: true, new: true}).then(console.log).catch(console.log);
+}, {upsert: true, setDefaultsOnInsert: true, new: true}).then(res => {}).catch(console.log);
 
 Reminder.findOneAndUpdate({_id: '565781bba17d0e685f8e2087'}, {
   name: 'Job 2',
@@ -47,4 +47,4 @@ Reminder.findOneAndUpdate({_id: '565781bba17d0e685f8e2087'}, {
     startAt: new Date(),
     interval: '* * * * * *'
   }
-}, {upsert: true, setDefaultsOnInsert: true, new: true}).then(console.log).catch(console.log);
+}, {upsert: true, setDefaultsOnInsert: true, new: true}).then(res => {}).catch(console.log);
